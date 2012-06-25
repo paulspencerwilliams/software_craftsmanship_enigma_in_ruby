965c05fc8a1b045bcb5eab3058616e6cf249d394
Paul Williams <paulswilliams@me.com>
2012-06-25 17:25:27 +0100

    encoder running letters through rotors

--- ruby_2/lib/encoder.rb
+++ ruby_2/lib/encoder.rb
@@ -1,7 +1,25 @@
+require './lib/reflector.rb'
+
 class Encoder 
-  attr_accessor :rotors
+  attr_accessor :rotors, :reflector
   
-  def initialize (rotors)
+  def initialize (rotors, reflector = Reflector.new)
     @rotors = rotors
+    @reflector = reflector
+  end
+  
+  def convert(letter) 
+    i = letter
+    rotors.reverse_each do | rotor |
+      i = rotor.convert(i)
+    end
+    
+    i = reflector.reflect(i)
+    
+    rotors.each do | rotor|
+      i = rotor.reverse(i)
+    end
+
+    i
   end
 end
