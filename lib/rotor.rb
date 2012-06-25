bc2047a8734bb0a54f38fba80f6852b25713ddd2
Paul Williams <paulswilliams@me.com>
2012-06-25 21:58:19 +0100

    rotors can now accept setPosition

--- lib/rotor.rb
+++ lib/rotor.rb
@@ -1,6 +1,10 @@
 class Rotor
-  attr_accessor :name
+  attr_accessor :name, :position
   def initialize name
     @name = name
+  end
+  
+  def setPosition(position)
+    @position = position
   end
 end
