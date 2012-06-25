b0de0083bc5be4e4ffe23eea0912d56c4edb3882
Paul Williams <paulswilliams@me.com>
2012-06-25 22:36:24 +0100

    enigmamachine delegates to encoder for encoding

--- lib/enigma_machine.rb
+++ lib/enigma_machine.rb
@@ -1,12 +1,23 @@
 require './lib/rotor_factory.rb'
+require './lib/encoder_factory.rb'
 
 class EnigmaMachine
   attr_accessor :rotors
   
-  def initialize rotorsToUse, rotorPositions, rotorFactory = RotorFactory.new
+  def initialize rotorsToUse, rotorPositions, rotorFactory = RotorFactory.new, encoderFactory = EncoderFactory.new
     @rotors = rotorFactory.createRotors(rotorsToUse)
+    @encoderFactory = encoderFactory
     for i in 0..rotorsToUse.length - 1 do
       @rotors[i].setPosition rotorPositions[i]
     end
+  end
+  
+  def type (input)
+    encoder = @encoderFactory.create
+    output = ""
+    input.split("").each do |i|
+      output << encoder.convert(i)
+    end
+    output
   end
 end
--- spec/enigma_machine_spec.rb
+++ spec/enigma_machine_spec.rb
@@ -1,19 +1,19 @@
 require './lib/enigma_machine.rb'
 
 describe EnigmaMachine do
+  before (:each) do
+    @rotorFactory = double("rotorFactory")
+    @encoder = double("encoder")
+    @rotorI = double("rotorI")
+    @rotorII = double("rotorII")
+    @rotorIII = double("rotorIII")
+    @rotors = [@rotorI, @rotorII, @rotorIII]
+    
+    @rotorFactory.stub(:createRotors).with(["I","II","III"]).and_return(@rotors)
+  end
+    
   context "setting up enigma machine " do
-    before do
-      @rotorFactory = double("rotorFactory")
-      @encoder = double("encoder")
-      @rotorI = double("rotorI")
-      @rotorII = double("rotorII")
-      @rotorIII = double("rotorIII")
-      @rotors = [@rotorI, @rotorII, @rotorIII]
-      
-      @rotorFactory.stub(:createRotors).with(["I","II","III"]).and_return(@rotors)
-      
 
-    end
     
     it "should setup rotors" do
       @rotorI.stub(:setPosition)
@@ -32,4 +32,24 @@ describe EnigmaMachine do
       enigmaMachine = EnigmaMachine.new ["I","II","III"], ["M","C","K"], @rotorFactory      
     end    
   end
+
+  context "when typing the message 'ABC'" do
+    it "should return 'DEF'" do
+      @rotorI.stub(:setPosition)
+      @rotorII.stub(:setPosition)
+      @rotorIII.stub(:setPosition)
+      
+      encoderFactory = double("encoderFactory")
+      encoder = double("encoder")
+      
+      encoderFactory.stub(:create).and_return(encoder)
+      encoder.stub(:convert).with("A").and_return("D")
+      encoder.stub(:convert).with("B").and_return("E")
+      encoder.stub(:convert).with("C").and_return("F")
+      
+      enigmaMachine = EnigmaMachine.new ["I","II","III"], ["M","C","K"], @rotorFactory, encoderFactory
+      enigmaMachine.type("ABC").should eq("DEF")
+    end
+  end
+  
 end
