c82fee3477a687eedb116766d2d2f685fa4941a1
Paul Williams <paulswilliams@me.com>
2012-06-26 20:54:10 +0100

    passing values through rotors right to left

--- lib/rotor.rb
+++ lib/rotor.rb
@@ -1,10 +1,21 @@
+require './lib/mappings.rb'
+
 class Rotor
-  attr_accessor :name, :position
+  include Mappings
+  attr_accessor :name
   def initialize name
     @name = name
   end
   
   def setPosition(position)
-    @position = position
+    @position = alphabet[position]
+  end
+  
+  def convert (letter)
+    inputIndex = alphabet[letter]
+    shiftedIndex = (inputIndex + @position) %26
+    convertedIndex = convertMappings[@name][shiftedIndex]
+    puts(convertedIndex)
+    alphabet[((26 - @position) + convertedIndex) %26] 
   end
 end
--- spec/rotor_spec.rb
+++ spec/rotor_spec.rb
@@ -1,11 +1,43 @@
 require './lib/rotor.rb'
 
 describe Rotor do
-  context "when setting position" do
-    it "should remember position" do
-      rotor  = Rotor.new "I" 
-      rotor.setPosition("B")
-      rotor.position.should eq("B")
+  
+  context "when rotor III is set to L" do
+    rotor = Rotor.new "III"
+    rotor.setPosition("L")
+    it "should convert E to T" do
+      rotor.convert("E").should eq("T")
     end
+    
+    it "should convert B to O" do
+      rotor.convert("B").should eq("O")
+    end
+    
+    it "should convert U to A" do
+      rotor.convert("U").should eq("A")
+    end
+  
   end
+  
+  
+  context "when rotor II is set to C" do
+    rotor = Rotor.new "II"
+    rotor.setPosition("C")
+    
+    it "should convert T to Y" do
+      rotor.convert("T").should eq("W")
+    end
+  end  
+
+  context "when rotor I is set to M" do
+    rotor = Rotor.new "I"
+    rotor.setPosition("M")
+    
+    it "should convert W to J" do
+      rotor.convert("W").should eq("J")
+    end
+  end  
+
+
+  
 end
